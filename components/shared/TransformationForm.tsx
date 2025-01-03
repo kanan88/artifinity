da04117e3ce30c318c0bf9255fdf7a3f57bfe79f
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-01-03 17:15:32 +0100

    implement all AI features

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -172,7 +172,7 @@ const TransformationForm = ({
           [fieldName === "prompt" ? "prompt" : "to"]: value,
         },
       }));
-    }, 1000);
+    }, 1000)();
 
     return onChangeField(value);
   };
@@ -220,6 +220,7 @@ const TransformationForm = ({
                 onValueChange={(value) =>
                   onSelectFieldHandler(value, field.onChange)
                 }
+                value={field.value}
               >
                 <SelectTrigger className="select-field">
                   <SelectValue placeholder="Select size" />
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
